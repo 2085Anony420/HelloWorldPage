181f9365c1d323e983b797885dad62df94c8be0e
2085Anony420 <159390615+2085Anony420@users.noreply.github.com>
2024-03-01 14:14:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showPopup(popup1) {
+    var popup = document.getElementById(popup1);
+    if (popup) {
+        popup.style.display = "block";
+    }
+}
+
+function hidePopup(popup1) {
+    var popup = document.getElementById(popup1);
+    if (popup) {
+        popup.style.display = "none";
+    }
+}
+
+function showPopup(popup2) {
+    var popup = document.getElementById(popup2);
+    if (popup) {
+        popup.style.display = "block";
+    }
+}
+
+function hidePopup(popup2) {
+    var popup = document.getElementById(popup2);
+    if (popup) {
+        popup.style.display = "none";
+    }
+}
